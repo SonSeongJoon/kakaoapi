5eb3eb7a648808417242a8bc71ea6011c7d316b9
SoengJoon <sonsj97@gmail.com>
2023-10-07 21:22:58 +0900

    modify

---  kakao.js
+++  kakao.js
@@ -1,59 +1,49 @@
-const https = require('https');
-const querystring = require('querystring');
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
 
-// 카카오 API 정보와 수신자 정보를 포함한 데이터 정의
-const data = {
-	username: 'seoulir07',
-	key: 'JAiY0S3262X8W94',
-	kakao_plus_id: '@seoulir07',
-	user_template_no: '20',
-	receiver: JSON.stringify([
-		{
-			name: '손성준',
-			mobile: '01028184783',
-			note1: '다이렉트센드 1',
-			note2: '다이렉트센드 2',
-			note3: '다이렉트센드 3',
-			note4: '다이렉트센드 4',
-		}
-		// 다른 수신자 정보도 여기에 추가하세요.
-	])
-};
+const app = express();
+const PORT = 5000;
 
-// HTTP 요청 옵션 정의
-const options = {
-	hostname: 'directsend.co.kr',
-	path: '/index.php/api_v2/kakao_notice',
-	method: 'POST',
-	port: 5000,
-	headers: {
-		'Content-Type': 'application/json;charset=utf-8',
-		'Accept': 'application/json'
+// API 키와 사용자 이름은 환경 변수 또는 설정 파일에서 가져옵니다.
+const USERNAME = process.env.DIRECTSEND_USERNAME || "seoulir07";
+const API_KEY = process.env.DIRECTSEND_API_KEY || "JAiY0S3262X8W94";
+const KAKAO_PLUS_ID = "@seoulir07";
+const USER_TEMPLATE_NO = "20";
+
+app.use(bodyParser.json());
+
+app.get("/send_kakao", async (req, res) => {
+	try {
+		const apiUrl = "https://directsend.co.kr/index.php/api_v2/kakao_notice";
+
+		const response = await axios.post(apiUrl, {
+			username: USERNAME,
+			key: API_KEY,
+			type: "node",
+			kakao_plus_id: KAKAO_PLUS_ID,
+			user_template_no: USER_TEMPLATE_NO,
+			receiver: [
+				{"name": "손성준", "mobile":"01028184783", "note1":"다이렉트센드 1", "note2":"다이렉트센드 2", "note3":"다이렉트센드 3", "note4":"다이렉트센드 4"},
+			]
+		}, {
+			headers: {
+				"Content-Type": "application/json;charset=utf-8",
+				"Accept": "application/json",
+				"Cache-Control": "no-cache"
+			}
+		});
+
+		// 성공/실패 여부를 클라이언트에 전달합니다.
+		res.json(response.data);
+	} catch (error) {
+		console.error("Error:", error);
+		res.status(500).send("Internal Server Error");
 	}
-};
-
-// HTTP 요청 보내기
-const req = https.request(options, (res) => {
-	let data = '';
-
-	// 응답 데이터를 문자열로 합치기
-	res.on('data', (chunk) => {
-		data += chunk;
-	});
-
-	// 응답이 종료되면 로그 출력
-	res.on('end', () => {
-		console.log('Response:', data);
-	});
 });
 
-// 오류 처리
-req.on('error', (e) => {
-	console.error('Error:', e.message);
+
+
+	app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-// 데이터를 JSON 문자열로 변환하고 요청 본문에 포함
-req.write(JSON.stringify(data));
-
-// 요청 종료
-req.end();
