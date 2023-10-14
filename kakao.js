b974bd35b63b3e963bd46f486566e2e5828b131e
SoengJoon <sonsj97@gmail.com>
2023-10-14 23:19:03 +0900

    send_kakao_modify : 함수 완성

--- kakao.js
+++ kakao.js
@@ -79,6 +79,38 @@ app.get("/send_kakao_agree/name/:name/phoneNum/:phoneNum/title/:title/state/:sta
 	}
 });
 
+app.get("/send_kakao_modify/name/:name/phoneNum/:phoneNum/file/:file/link/:link", async (req, res) => {
+	const {name, phoneNum, file, link} = req.params;
+	try {
+		const apiUrl = "https://directsend.co.kr/index.php/api_v2/kakao_notice";
+		const response = await axios.post(apiUrl, {
+			username        : USERNAME,
+			key             : API_KEY,
+			type            : "node",
+			kakao_plus_id   : KAKAO_PLUS_ID,
+			user_template_no: 53,
+			receiver        : [
+				{"name"    : name,
+					"mobile": phoneNum,
+					"note1" : name,
+					"note2" : file,
+					"note3" : "https://seouliredsm.netlify.app/receive/detail/"+link,
+				},
+			]
+		}, {
+			headers: {
+				"Content-Type" : "application/json;charset=utf-8",
+				"Accept"       : "application/json",
+				"Cache-Control": "no-cache"
+			}
+		});
+
+		res.json(response.data);
+	} catch (error) {
+		console.error("Error:", error);
+		res.status(500).send("Internal Server Error");
+	}
+});
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
